e7c9212300e7f615faed0206ca4fe43d609fb924
flis_ann <ann.flismark@gmail.com>
2020-11-09 20:34:44 +0100

    creating test for index action

--- config/application.rb
+++ config/application.rb
@@ -25,11 +25,11 @@ module RailsDemo
     config.generators do |generate|
       generate.helper false
       generate.assets false
-      generate.view.specs false
+      generate.view_specs false
       generate.helper_specs false
       generate.routing_specs false
       generate.controller_specs false
-      genereate.request_specs false
+      generate.request_specs false
     end
   end
 end
