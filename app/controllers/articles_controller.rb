21414185777d4d115d5ef843f68bf4560198ad7e
flis_ann <ann.flismark@gmail.com>
2020-11-10 10:03:20 +0100

    Adding test for verifying an article has link and content as well as code for that

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,9 @@
 class ArticlesController < ApplicationController
   def index
-    @articles = Article.all #goes and gets all articles from db. AFter it is fetched it should send it to views to disply them
+    @articles = Article.all #goes and gets all articles from db. After it is fetched it should send it to views to disply them
   end
-
+  
+  def show
+    @article = Article.find(params[:id]) 
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
   root controller: :articles, action: :index  
+  resources :articles, only: [:show] #get a single item, get a list of item is index action
 end
